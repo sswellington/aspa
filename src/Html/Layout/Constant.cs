456b3f9d21f3a9a26f05ccbc858213a1e3dbcc6f
sswellington <sswellington@pm.me>
2025-06-18 16:24:56 -0300

    feat: css

--- src/Html/Layout/Constant.cs
+++ src/Html/Layout/Constant.cs
@@ -10,8 +10,12 @@ public static class Constant
         <meta http-equiv="Cache-control" content="public">
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        
+    
         <title></title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
+        <link rel="stylesheet" href="https://newcss.net/theme/terminal.css">
       </head>
       <body></body>
     </html>
